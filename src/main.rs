ce3297536803cd71eb12e9d02b40ea3c9614446d
Nigel Baillie <nigel@baillie.dev>
2023-08-11 22:10:44 +0900

    It basically works now

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,180 @@
+use std::collections::{hash_map, HashMap};
 use std::convert::Infallible;
-use std::time::Duration;
 
-use hyper::body::Bytes;
+use hyper::body::{Bytes, HttpBody};
+use hyper::http::request::Parts;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, Server, Uri, HeaderMap};
+
+use lazy_static::lazy_static;
+use tokio::sync::{mpsc, RwLock};
 
 use uuid::Uuid;
 
-async fn serve(request: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let (parts, _req_body) = request.into_parts();
-    let id = Uuid::new_v4();
+struct Pipe {
+    id: Uuid,
+    path: String,
+    senders: Vec<mpsc::Sender<PipeEntry>>,
+    last_received: Option<PipeEntry>,
+}
 
+#[derive(Clone)]
+struct PipeEntry {
+    uri: Uri,
+    method: Method,
+    headers: HeaderMap,
+    body: Bytes,
+}
+
+struct Pipes {
+    by_path: HashMap<String, Pipe>,
+    counter: usize,
+}
+
+lazy_static! {
+    static ref PIPES: RwLock<Pipes> = RwLock::new(Pipes {
+        by_path: HashMap::new(),
+        counter: 0,
+    });
+}
+
+async fn serve(request: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let (parts, req_body) = request.into_parts();
+
+    let id = Uuid::new_v4();
     println!("{id} {} {}", parts.method, parts.uri.path());
 
-    let (mut sender, body) = Body::channel();
+    if parts.method == Method::GET {
+        return serve_get(id, parts.uri.clone()).await;
+    } else {
+        return serve_post(id, parts, req_body).await;
+    }
+}
+
+async fn serve_post(id: Uuid, parts: Parts, mut body: Body) -> Result<Response<Body>, Infallible> {
+    let mut senders_to_delete = Vec::<bool>::with_capacity(128);
+    let mut need_to_delete = false;
+    let mut valid_senders_present = false;
+    let pipe_counter;
+    {
+        let pipes = PIPES.read().await;
+        let Some(pipe) = pipes.by_path.get(parts.uri.path()) else {
+            return Ok(Response::new(Body::empty()));
+        };
+        pipe_counter = pipes.counter;
+
+        let mut data_chunks = Vec::<Bytes>::new();
+        while let Some(data) = body.data().await {
+            match data {
+                Ok(incoming_bytes) => {
+                    data_chunks.push(incoming_bytes);
+                }
+                Err(e) => {
+                    println!("{id} failed to receive body ({e:?})");
+                    return Ok(Response::new(Body::empty()));
+                }
+            }
+        }
+        let total_bytes = data_chunks.concat();
+
+        let entry = PipeEntry {
+            uri: parts.uri.clone(),
+            method: parts.method.clone(),
+            headers: parts.headers.clone(),
+            body: Bytes::copy_from_slice(&total_bytes),
+        };
+
+        for sender in pipe.senders.iter() {
+            if let Err(e) = sender.send(entry.clone()).await {
+                println!("{id} deleting listener {} ({e:?})", pipe.id);
+                senders_to_delete.push(true);
+                need_to_delete = true;
+            } else {
+                senders_to_delete.push(false);
+                valid_senders_present = true;
+            }
+        }
+    }
+
+    if need_to_delete {
+        let mut pipes = PIPES.write().await;
+
+        if pipes.counter != pipe_counter {
+            return Ok(Response::new(Body::empty()));
+        }
+        pipes.counter += 1;
+
+        if valid_senders_present {
+            let Some(pipe) = pipes.by_path.get_mut(parts.uri.path()) else {
+                return Ok(Response::new(Body::empty()));
+            };
+            let mut new_senders: Vec<mpsc::Sender<PipeEntry>> = Vec::with_capacity(senders_to_delete.len());
+
+            for (i, delete) in senders_to_delete.iter().enumerate() {
+                if !delete {
+                    new_senders.push(pipe.senders[i].clone());
+                }
+            }
+
+            pipe.senders = new_senders;
+        } else {
+            pipes.by_path.remove(parts.uri.path());
+        }
+    }
+
+    Ok(Response::new(Body::empty()))
+}
+
+async fn serve_get(id: Uuid, uri: Uri) -> Result<Response<Body>, Infallible> {
+    let path = uri.path().to_string();
+
+    let (mut body_sender, body) = Body::channel();
+
+    {
+        let vec = format!("Your URL is {uri}\ncurl -d \"Hello World!\" {uri}\n\n").into_bytes();
+        let _ = body_sender.send_data(Bytes::copy_from_slice(&vec)).await;
+    }
+
+    let (pipe_sender, mut pipe_receiver) = mpsc::channel::<PipeEntry>(128);
+    {
+        let mut pipes = PIPES.write().await;
+        let pipe = match pipes.by_path.entry(path.clone()) {
+            hash_map::Entry::Occupied(o) => o.into_mut(),
+            hash_map::Entry::Vacant(v) => v.insert(Pipe {
+                id: id.clone(),
+                path: path.clone(),
+                senders: Vec::new(),
+                last_received: None,
+            }),
+        };
+        pipe.senders.push(pipe_sender);
+    }
 
     tokio::spawn(async move {
-        loop {
-            tokio::time::sleep(Duration::from_millis(1000)).await;
-            if let Err(e) = sender.send_data(Bytes::from_static(b"hello\n")).await {
-                println!("{id} Cut off {:?}", e);
+        macro_rules! send {
+            ($e:expr) => {
+                {
+                    let vec = $e;
+                    if let Err(e) = body_sender.send_data(Bytes::copy_from_slice(&vec)).await {
+                        println!("{id} failed to send to listener {:?}", e);
+                        break;
+                    }
+                }
+            };
+        }
+
+        while let Some(entry) = pipe_receiver.recv().await {
+            send!(format!("{} {}\n", entry.method, entry.uri).into_bytes());
+            for (key, value) in entry.headers.iter() {
+                send!(format!("{key}: {value:?}\n").into_bytes());
+            }
+            send!(*b"\n");
+
+            if let Err(e) = body_sender.send_data(entry.body).await {
+                println!("{id} failed to send to listener {:?}", e);
                 break;
             }
+            send!(*b"\n\n");
         }
     });
 
